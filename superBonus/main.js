d4763cae8934dcc104e5ae4ebb10bc1b9c822bf9
Simone <simonelega.sl@gmail.com>
2020-01-18 13:57:58 +0100

    Cartella superBonus. l'immagine dello slider cambia cliccando sui pallin

---
+++ superBonus/main.js
@@ -0,0 +1,66 @@
+$(document).ready(
+  function(){
+
+    var prev = $('.prev');
+    var next = $('.next');
+    // creo la funzione per andare avanti
+    next.click(function(){
+      // variabili slider
+      var imgActive = $('img.active');
+      var imageNext = $('img.active').next();
+      // variabili circles
+      var circleActive = $('i.active');
+      var circleNext = $('i.active').next();
+      // motore slider
+      imgActive.removeClass('active');
+      imageNext.addClass('active');
+      if(imgActive.hasClass('last')==true){
+        imgActive.removeClass('active');
+        $('img.first').addClass('active');
+      }
+      // motore circle
+      circleActive.removeClass('active');
+      circleNext.addClass('active');
+      if(circleActive.hasClass('last')==true){
+        circleActive.removeClass('active');
+        $('i.first').addClass('active');
+      }
+    });
+
+    // creo la funzione per tornare indietro
+    prev.click(function(){
+      // variabili slider
+      var imgActive = $('img.active');
+      var imagePrev = $('img.active').prev();
+      // variabili circles
+      var circleActive = $('i.active');
+      var circlePrev = $('i.active').prev();
+      // motore slider
+      imgActive.removeClass('active');
+      imagePrev.addClass('active');
+      if(imgActive.hasClass('first')==true){
+        imgActive.removeClass('active');
+        $('img.last').addClass('active');
+      }
+      // motore circle
+      circleActive.removeClass('active');
+      circlePrev.addClass('active');
+      if(circleActive.hasClass('first')==true){
+        circleActive.removeClass('active');
+        $('i.last').addClass('active');
+      }
+    });
+  }
+);
+
+$('.nav i').click(function(){
+  $('.nav i').removeClass('active');
+  $(this).addClass('active');
+  var position = $(this).index();
+  var images = $('.images > img');
+  var imgreplace = $('.images > img').eq(position);
+  images.removeClass('active');
+  imgreplace.addClass('active');
+
+  console.log(imgreplace);
+});
